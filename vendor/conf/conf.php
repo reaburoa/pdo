0f106009e513fcc908f8d041e775937d24d8bff7
Rean <zhanglei@pyyx.com>
2017-07-16 15:08:02 +0800

    change conf

--- vendor/conf/conf.php
+++ vendor/conf/conf.php
@@ -3,13 +3,13 @@
 return [
     'a' => [
         'dsn' => 'mysql:host=127.0.0.1;port=3306;charset=utf8mb4',
-        'user' => 'root',
-        'password' => 'reaburoa'
+        'user' => 'user',
+        'password' => '****'
     ],
     'b' => [
         'dsn' => 'mysql:host=127.0.0.1;port=3306;charset=utf8mb4',
-        'user' => 'root',
-        'password' => 'reaburoa'
+        'user' => 'user',
+        'password' => '****'
     ],
     'test' => 'a|b|a|b',
     'i_test' => 'a|b|a|b',
