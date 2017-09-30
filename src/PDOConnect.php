38d9d40bf00752babcc33b77bac6ac07abb64284
Rean <zhanglei@pyyx.com>
2017-09-30 19:40:51 +0800

    优化

--- src/PDOConnect.php
+++ src/PDOConnect.php
@@ -42,6 +42,11 @@ class PDOConnect
     private static $default_charset = 'utf8mb4';
 
     /**
+     * conf file dir
+     */
+    private static $conf_dir = dirname(dirname(__FILE__)).'/conf';
+
+    /**
      * PDO connect timeout default 5 seconds
      */
     private static $timeout = 5;
@@ -110,7 +115,7 @@ class PDOConnect
     private static function getConf()
     {
         if (empty(self::$origin_conf)) {
-            self::$origin_conf = require_once(CONF_PATH.'/conf.php');
+            self::$origin_conf = require_once(self::$conf_dir.'/conf.php');
         }
         if (!isset(self::$origin_conf[self::$cluster])) {
             throw new \Exception("Conf has no ".self::$cluster." cluster");
