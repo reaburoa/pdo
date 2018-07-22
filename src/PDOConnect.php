1eb6c217f3aa40ef7d522b0242ec1e1910e35d99
Rean <lei.zhang12@yintech.cn>
2018-07-22 13:18:01 +0800

    配置文件读取位置便更改

--- src/PDOConnect.php
+++ src/PDOConnect.php
@@ -110,7 +110,7 @@ class PDOConnect
     private static function getConf()
     {
         if (empty(self::$origin_conf)) {
-            self::$origin_conf = require_once(dirname(dirname(__FILE__)).'/conf/conf.php');
+            self::$origin_conf = require_once(dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/conf/conf.php');
         }
         if (!isset(self::$origin_conf[self::$cluster])) {
             throw new \Exception("Conf has no ".self::$cluster." cluster");
